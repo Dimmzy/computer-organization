899d92b5647e60bf62efe0c0e7864ca05438e864
Dima <leetsushi@gmail.com>
2020-01-15 13:11:35 +0200

    Finished jump table

--- ex3/compile.c
+++ ex3/compile.c
@@ -5,31 +5,30 @@
 #define MAX_LEN 50
 
 void writeHeaders(FILE* wFile);
-void writeJT(FILE* wFile, int* jumps, int num);
-int addSwitchCases(FILE* rFile, int* arr);
-int compare (const void * a, const void * b);
+void writeJT(FILE* wFile, int* cases, int size,int max);
+int getMax(int* arr,int size);
+int addSwitchCases(FILE* rFile,FILE* wFile, int* arr);
+int cmp(const void * a, const void * b);
 
-int main() {
+    int main() {
   FILE *readFile, *writeFile;
   readFile = fopen("switch.c","r");
   writeFile = fopen("switch.s", "w");
   writeHeaders(writeFile);
   int* switchCases = malloc(INITIAL_CAPACITY * sizeof(int));
-  int size = addSwitchCases(readFile,switchCases);
+  int size = addSwitchCases(readFile,writeFile,switchCases);
+  int max = getMax(switchCases,size);
   int i;
   for(i = 0; i < size; i++)
     printf("Case number %d, is %d\n", i,switchCases[i]);
+  writeJT(writeFile,switchCases,size, max);
   return 0;
 }
 
 void writeHeaders(FILE* wFile) {
   fprintf(wFile, ".section\t.text\n");
   fprintf(wFile, ".globl\tswitch2\n");
-  fprintf(wFile, "switch2: ");
-  fprintf(wFile, "movl $0, %%eax\n"); // Result = 0
-  fprintf(wFile, "movl %%edi, %%ebx\n"); // long *p1
-  fprintf(wFile, "movl %%esi, %%ecx\n"); // long *p2
-  fprintf(wFile, "movl %%edi, %%edx\n"); // long action
+  fprintf(wFile, "switch2:\n");
 }
 
 /**
@@ -38,7 +37,7 @@ void writeHeaders(FILE* wFile) {
  * @param arr the cases array we write to
  * @return number of cases written
  */
-int addSwitchCases(FILE* rFile,int* arr) {
+int addSwitchCases(FILE* rFile,FILE* wFile, int* arr) {
   int size = 0;
   int capacity = INITIAL_CAPACITY;
   char buffer[MAX_LEN + 1];
@@ -60,25 +59,69 @@ int addSwitchCases(FILE* rFile,int* arr) {
       size++;
     }
   }
-  // Sorts the array using QuickSort.
-  qsort(arr,size,sizeof(int),compare);
   // Reduce each element by the smallest one in the array
   int i;
   int min = arr[0];
+  int max = arr[0];
+  for (i = 1; i < size; i++) { // Find min and max of cases array
+    if (arr[i] < min)
+      min = arr[i];
+    if (arr[i] > max)
+      max = arr[i];
+  }
   for (i = 0; i < size; i++)
     arr[i] -= min;
+  fprintf(wFile,"\tsubq\t$%d, %%rdx\n",min); // Subtracts the min element from the action
+  fprintf(wFile,"\tcmpq\t$%d, %%rdx\n",max-min); // Checks if the argument is higher than maximal case
   return size;
 }
 
-void writeJT(FILE* wFile, int* jumps, int num) {
-  fprintf(wFile, ".section\t.rodata\n");
-  fprintf(wFile, ".align\t8\n");
+/**
+ * Writes the Jump Table elements, iterates from 0 to max case, if n isn't in our array, jump to default
+ */
+void writeJT(FILE* wFile, int* cases, int size, int max) {
+  fprintf(wFile, "\n.section\t.rodata\n"); // Jump table headers
+  fprintf(wFile, "\t.align\t8\n");
+  fprintf(wFile,".L4:\n");
+  qsort(cases,size,sizeof(int),cmp);
   int i;
-  for (i = 0; i < num; i++) {
-    fprintf(wFile, ".quad\t.L%d\n", jumps[i]);
+  int j = 0;
+  int label = 3; // Labels start at 3
+  for (i = 0; i <= max; i++) {
+    if (cases[j] == i) { // Checks if the case is in our cases array, if so we jump to it's label
+      fprintf(wFile,"\t.quad\t.L%d\n",label++);
+      if (label == 4) // Skip label = 4 since it's our JMP table label.
+        label++;
+      j++;
+    } else { // If the case is not in our array we jump to the default label
+      fprintf(wFile,"\t.quad\t.L%d\n",max);
+    }
   }
 }
 
-int compare (const void * a, const void * b) {
+/**
+ * Returns maximal element from the array
+ * @param arr the array we check
+ * @param size size of the array
+ * @return the value of the maximal element in the array
+ */
+int getMax(int *arr,int size) {
+  int i;
+  int max = arr[0];
+  for(i = 1; i < size; i++) {
+    if (arr[i] > max)
+      max = arr[i];
+  }
+  return max;
+}
+
+/**
+ * Compares to ints and returns the higher one
+ * @param a first int to compare
+ * @param b second int to compare
+ * @return the higher number
+ */
+int cmp (const void * a, const void * b) {
   return ( *(int*)a - *(int*)b );
 }
+
